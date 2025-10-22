db9c967bd0df26c9b1ed37a70ce2780a2da9233f
Gadu25 <alexanderudag@gmail.com>
2025-10-22 10:21:05 +0800

    scafolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  const [count, setCount] = useState(0)
+  useGSAP(() => {
+    gsap.fromTo('.test', {
+      y: 20,
+      opacity: 0
+    },{
+      y: 0,
+      opacity: 1,
+      ease: 'power1.out',
+      duration: 1,
+      stagger: 0.2
+    })
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex-center h-[100vh]">
+        <h1 className='test text-gray-400'>Hello World</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
